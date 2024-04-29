5860b861c9207ef96cf78fa114741df3765a398c
Lalit sharma <geekymonkcp742k@gmail.com>
2024-04-29 23:42:36 +0530

    🛡  feat( RootProviders Tanstack React Query Client): Integrate React Query for data fetching. Includes global provider (RootProviders) and React Query developer tools (disabled by default).

--- components/providers/RootProviders.tsx
+++ components/providers/RootProviders.tsx
@@ -1,23 +1,25 @@
 "use client";
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React, { ReactNode } from "react";
 
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { ThemeProvider } from "next-themes";
 
 function RootProviders({ children }: { children: ReactNode }) {
-
+    const [queryClient] = React.useState(() => new QueryClient({}));
     return (
-
-        <ThemeProvider
-            attribute="class"
-            defaultTheme="dark"
-            enableSystem
-            disableTransitionOnChange
-        >
-            {children}
-        </ThemeProvider>
-
-
+        <QueryClientProvider client={queryClient}>
+            <ThemeProvider
+                attribute="class"
+                defaultTheme="dark"
+                enableSystem
+                disableTransitionOnChange
+            >
+                {children}
+            </ThemeProvider>
+            <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
     );
 }
 
