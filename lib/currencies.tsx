7f5879d2b82f73dd5ce2124472ae6e324a506d72
Lalit sharma <geekymonkcp742k@gmail.com>
2024-04-30 12:34:28 +0530

    💱 feat(Currencies lib): Create Currencies list with common currencies (INR , USD, EUR, JPY, etc.) including value, label, and locale.

---
+++ lib/currencies.tsx
@@ -0,0 +1,124 @@
+export const Currencies = [
+    {
+        value: "INR",
+        label: "₹ Rupee",
+        locale: "en-IN",
+    },
+    {
+        value: "USD",
+        label: "$ Dollar",
+        locale: "en-US",
+    },
+    {
+        value: "EUR",
+        label: "€ Euro",
+        locale: "de-DE",
+    },
+    {
+        value: "JPY",
+        label: "¥ Yen",
+        locale: "ja-JP",
+    },
+    {
+        value: "GBP",
+        label: "£ Pound",
+        locale: "en-GB",
+    },
+    {
+        value: "AUD",
+        label: "$ Dollar",
+        locale: "en-AU",
+    },
+    {
+        value: "CAD",
+        label: "$ Dollar",
+        locale: "en-CA",
+    },
+    {
+        value: "CHF",
+        label: "Fr. Franc",
+        locale: "de-CH",
+    },
+    {
+        value: "CNY",
+        label: "¥ Yuan",
+        locale: "zh-CN",
+    },
+    {
+        value: "SEK",
+        label: "kr Krona",
+        locale: "sv-SE",
+    },
+    {
+        value: "NZD",
+        label: "$ Dollar",
+        locale: "en-NZ",
+    },
+    {
+        value: "KRW",
+        label: "₩ Won",
+        locale: "ko-KR",
+    },
+    {
+        value: "SGD",
+        label: "$ Dollar",
+        locale: "en-SG",
+    },
+    {
+        value: "NOK",
+        label: "kr Krone",
+        locale: "no-NO",
+    },
+    {
+        value: "MXN",
+        label: "$ Peso",
+        locale: "es-MX",
+    },
+    {
+        value: "BRL",
+        label: "R$ Real",
+        locale: "pt-BR",
+    },
+    {
+        value: "INR",
+        label: "₹ Rupee",
+        locale: "en-IN",
+    },
+    {
+        value: "RUB",
+        label: "₽ Ruble",
+        locale: "ru-RU",
+    },
+    {
+        value: "ZAR",
+        label: "R Rand",
+        locale: "en-ZA",
+    },
+    {
+        value: "TRY",
+        label: "₺ Lira",
+        locale: "tr-TR",
+    },
+    {
+        value: "HKD",
+        label: "$ Dollar",
+        locale: "en-HK",
+    },
+    {
+        value: "IDR",
+        label: "Rp Rupiah",
+        locale: "id-ID",
+    },
+    {
+        value: "MYR",
+        label: "RM Ringgit",
+        locale: "ms-MY",
+    },
+    {
+        value: "HUF",
+        label: "Ft Forint",
+        locale: "hu-HU",
+    },
+];
+
+export type Currency = (typeof Currencies)[0];
