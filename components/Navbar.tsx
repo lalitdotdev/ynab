378814d09502a16c215e2d2cc8569e35b69611a7
Lalit sharma <geekymonkcp742k@gmail.com>
2024-04-30 11:18:46 +0530

    👆 feat(Desktop Navbar) 💻 : Implement DesktopNavbar for larger screens. Displays Logo, links from items array, and aligns theme switcher and user button on the right.

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,14 +6,16 @@ import React, { useState } from "react";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 
 import Link from "next/link";
+import { Menu } from "lucide-react";
+import { ThemeSwitcherBtn } from "./ThemeSwitchBtn";
+import { UserButton } from "@clerk/nextjs";
 import { cn } from "@/lib/utils";
 import { usePathname } from "next/navigation";
 
 function Navbar() {
     return (
         <>
-            {/* <DesktopNavbar />
-            <MobileNavbar /> */}
+            <DesktopNavbar />
         </>
     );
 }
@@ -24,6 +26,32 @@ const items = [
     { label: "Manage", link: "/manage" },
 ];
 
+
+
+function DesktopNavbar() {
+    return (
+        <div className="hidden border-separate border-b bg-background md:block">
+            <nav className="container flex items-center justify-between px-8">
+                <div className="flex h-[80px] min-h-[60px] items-center gap-x-4">
+                    <Logo />
+                    <div className="flex h-full">
+                        {items.map((item) => (
+                            <NavbarItem
+                                key={item.label}
+                                link={item.link}
+                                label={item.label}
+                            />
+                        ))}
+                    </div>
+                </div>
+                <div className="flex items-center gap-2">
+                    <ThemeSwitcherBtn />
+                    <UserButton afterSignOutUrl="/sign-in" />
+                </div>
+            </nav>
+        </div>
+    );
+}
 
 function NavbarItem({
     link,
