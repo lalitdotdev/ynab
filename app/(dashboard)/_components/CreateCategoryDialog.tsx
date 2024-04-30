e76bc4f7b6be361c3b044f46856d6d3d48e325a8
Lalit sharma <geekymonkcp742k@gmail.com>
2024-04-30 15:56:17 +0530

    feat(CreateCategory Dialog): Implement robust CreateCategoryDialog with validation & component composition.

--- app/(dashboard)/_components/CreateCategoryDialog.tsx
+++ app/(dashboard)/_components/CreateCategoryDialog.tsx
@@ -1,16 +1,44 @@
 "use client";
 
+import { CircleOff, Loader2, PlusSquare } from "lucide-react";
+import { CreateCategorySchema, CreateCategorySchemaType } from "@/lib/validators/categories";
 import {
     Dialog,
+    DialogClose,
     DialogContent,
-    DialogTrigger
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
 } from "@/components/ui/dialog";
-import { ReactNode, useState } from "react";
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+} from "@/components/ui/form";
+import {
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
+} from "@/components/ui/popover";
+import React, { ReactNode, useState } from "react";
 
 import { Button } from "@/components/ui/button";
 import { Category } from "@prisma/client";
-import { PlusSquare } from "lucide-react";
+import { Input } from "@/components/ui/input";
+import Picker from "@emoji-mart/react";
 import { TransactionType } from "@/lib/types";
+import { cn } from "@/lib/utils";
+import data from "@emoji-mart/data";
+import { useForm } from "react-hook-form";
+import { useTheme } from "next-themes";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+;
 
 interface Props {
     type: TransactionType;
@@ -20,6 +48,17 @@ interface Props {
 
 function CreateCategoryDialog({ type, successCallback, trigger }: Props) {
     const [open, setOpen] = useState(false);
+    const form = useForm<CreateCategorySchemaType>({
+        resolver: zodResolver(CreateCategorySchema),
+        defaultValues: {
+            type,
+        },
+    });
+
+
+    const theme = useTheme();
+
+
 
 
     return (
@@ -38,12 +77,104 @@ function CreateCategoryDialog({ type, successCallback, trigger }: Props) {
                 )}
             </DialogTrigger>
             <DialogContent>
-                <div>
-                    Dialog Content here
-                </div>
+                <DialogHeader>
+                    <DialogTitle>
+                        Create
+                        <span
+                            className={cn(
+                                "m-1",
+                                type === "income" ? "text-emerald-500" : "text-red-500"
+                            )}
+                        >
+                            {type}
+                        </span>
+                        category
+                    </DialogTitle>
+                    <DialogDescription>
+                        Categories are used to group your transactions
+                    </DialogDescription>
+                </DialogHeader>
+                <Form {...form}>
+                    <form onSubmit={form.handleSubmit(() => { })} className="space-y-8">
+                        <FormField
+                            control={form.control}
+                            name="name"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Name</FormLabel>
+                                    <FormControl>
+                                        <Input placeholder="Category" {...field} />
+                                    </FormControl>
+                                    <FormDescription>
+                                        This is how your category will appear in the app
+                                    </FormDescription>
+                                </FormItem>
+                            )}
+                        />
+
+                        <FormField
+                            control={form.control}
+                            name="icon"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Icon</FormLabel>
+                                    <FormControl>
+                                        <Popover>
+                                            <PopoverTrigger asChild>
+                                                <Button
+                                                    variant={"outline"}
+                                                    className="h-[100px] w-full"
+                                                >
+                                                    {form.watch("icon") ? (
+                                                        <div className="flex flex-col items-center gap-2">
+                                                            <span className="text-5xl" role="img">
+                                                                {field.value}
+                                                            </span>
+                                                            <p className="text-xs text-muted-foreground">
+                                                                Click to change
+                                                            </p>
+                                                        </div>
+                                                    ) : (
+                                                        <div className="flex flex-col items-center gap-2">
+                                                            <CircleOff className="h-[48px] w-[48px]" />
+                                                            <p className="text-xs text-muted-foreground">
+                                                                Click to select
+                                                            </p>
+                                                        </div>
+                                                    )}
+                                                </Button>
+                                            </PopoverTrigger>
+
+                                        </Popover>
+                                    </FormControl>
+                                    <FormDescription>
+                                        This is how your category will appear in the app
+                                    </FormDescription>
+                                </FormItem>
+                            )}
+                        />
+                    </form>
+                </Form>
+                <DialogFooter>
+                    <DialogClose asChild>
+                        <Button
+                            type="button"
+                            variant={"secondary"}
+                            onClick={() => {
+                                form.reset();
+                            }}
+                        >
+                            Cancel
+                        </Button>
+                    </DialogClose>
+                    <Button onClick={form.handleSubmit(() => { })} disabled={false}>
+                        {!true && "Create"}
+                        {true && <Loader2 className="animate-spin" />}
+                    </Button>
+                </DialogFooter>
             </DialogContent>
         </Dialog>
     );
 }
 
-export default CreateCategoryDialog;/*  */
+export default CreateCategoryDialog;
