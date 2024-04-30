7df4bf136d59cdb498e1ace44c229ace9b0500ec
Lalit sharma <geekymonkcp742k@gmail.com>
2024-04-30 16:52:19 +0530

    ⚡API(createCategory in db): Implement server-side GET /categories endpoint (authenticated)

---
+++ app/api/categories/route.ts
@@ -0,0 +1,36 @@
+import { currentUser } from '@clerk/nextjs/server';
+import db from '@/lib/db';
+import { redirect } from 'next/navigation';
+import { z } from 'zod';
+
+export async function GET(request: Request) {
+  const user = await currentUser();
+  if (!user) {
+    redirect('/sign-in');
+  }
+
+  const { searchParams } = new URL(request.url);
+  const paramType = searchParams.get('type');
+
+  const validator = z.enum(['expense', 'income']).nullable();
+
+  const queryParams = validator.safeParse(paramType);
+  if (!queryParams.success) {
+    return Response.json(queryParams.error, {
+      status: 400,
+    });
+  }
+
+  const type = queryParams.data;
+  const categories = await db.category.findMany({
+    where: {
+      userId: user.id,
+      ...(type && { type }), // include type in the filters if it's defined
+    },
+    orderBy: {
+      name: 'asc',
+    },
+  });
+
+  return Response.json(categories);
+}
