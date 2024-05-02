6a42ef988d50a6527b7a5c3ab161aed046807b6f
Lalit sharma <geekymonkcp742k@gmail.com>
2024-05-02 09:49:41 +0530

    🛠 helpers(for consistent time representation): DateToUTCDate function (util) \- Leverages Date.UTC for robust conversion of local dates to UTC.\n- Ensures consistent time representation across the application.

---
+++ lib/helpers.ts
@@ -0,0 +1,16 @@
+
+
+export function DateToUTCDate(date: Date) {
+  return new Date(
+    Date.UTC(
+      date.getFullYear(),
+      date.getMonth(),
+      date.getDate(),
+      date.getHours(),
+      date.getMinutes(),
+      date.getSeconds(),
+      date.getMilliseconds(),
+    ),
+  );
+}
+
