211bb734474e1291d4826253f43b89cabd6ba3ce
Lalit sharma <geekymonkcp742k@gmail.com>
2024-05-30 16:49:57 +0530

    feat(HistoryPeriodSelector fetch): Add useQuery hook to fetch history periods for the overview page.

--- app/(dashboard)/_components/HistoryPeriodSelector.tsx
+++ app/(dashboard)/_components/HistoryPeriodSelector.tsx
@@ -20,21 +20,24 @@ function HistoryPeriodSelector({
     timeframe,
     setTimeframe,
 }: Props) {
-
+    const historyPeriods = useQuery<GetHistoryPeriodsResponseType>({
+        queryKey: ["overview", "history", "periods"],
+        queryFn: () => fetch(`/api/history-periods`).then((res) => res.json()),
+    });
 
     return (
         <div className="flex flex-wrap items-center gap-4">
-            {/* <SkeletonWrapper isLoading={historyPeriods.isFetching} fullWidth={false}> */}
-            <Tabs
-                value={timeframe}
-                onValueChange={(value) => setTimeframe(value as Timeframe)}
-            >
-                <TabsList>
-                    <TabsTrigger value="year">Year</TabsTrigger>
-                    <TabsTrigger value="month">Month</TabsTrigger>
-                </TabsList>
-            </Tabs>
-            {/* </SkeletonWrapper> */}
+            <SkeletonWrapper isLoading={historyPeriods.isFetching} fullWidth={false}>
+                <Tabs
+                    value={timeframe}
+                    onValueChange={(value) => setTimeframe(value as Timeframe)}
+                >
+                    <TabsList>
+                        <TabsTrigger value="year">Year</TabsTrigger>
+                        <TabsTrigger value="month">Month</TabsTrigger>
+                    </TabsList>
+                </Tabs>
+            </SkeletonWrapper>
 
         </div>
     );
