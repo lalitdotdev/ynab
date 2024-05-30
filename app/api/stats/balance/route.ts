fafc1b5f9d233991ae6e5b8fc41634d2b3275af9
Lalit sharma <geekymonkcp742k@gmail.com>
2024-05-30 14:57:32 +0530

    ⚡API: Implement GET endpoint for fetching balance statistics based on user transactions within a specified date range.

---
+++ app/api/stats/balance/route.ts
@@ -0,0 +1,50 @@
+import { OverviewQuerySchema } from '@/lib/validators/overview';
+import { currentUser } from '@clerk/nextjs/server';
+import db from '@/lib/db';
+import { redirect } from 'next/navigation';
+
+export async function GET(req: Request) {
+  const user = await currentUser();
+  if (!user) {
+    redirect('/sign-in');
+  }
+
+  const { searchParams } = new URL(req.url);
+  const from = searchParams.get('from');
+  const to = searchParams.get('to');
+
+  const queryParams = OverviewQuerySchema.safeParse({ from, to });
+
+  if (!queryParams.success) {
+    return Response.json(queryParams.error.message, {
+      status: 400,
+    });
+  }
+
+  const stats = await getBalanceStats(user.id, queryParams.data.from, queryParams.data.to);
+
+  return Response.json(stats);
+}
+
+export type GetBalanceStatsResponseType = Awaited<ReturnType<typeof getBalanceStats>>;
+
+async function getBalanceStats(userId: string, from: Date, to: Date) {
+  const totals = await db.transaction.groupBy({
+    by: ['type'],
+    where: {
+      userId,
+      date: {
+        gte: from,
+        lte: to,
+      },
+    },
+    _sum: {
+      amount: true,
+    },
+  });
+
+  return {
+    expense: totals.find((t) => t.type === 'expense')?._sum.amount || 0,
+    income: totals.find((t) => t.type === 'income')?._sum.amount || 0,
+  };
+}
