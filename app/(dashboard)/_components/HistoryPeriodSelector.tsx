76091a7a5d164f934acc3141cbc0f4139dd4bd1b
Lalit sharma <geekymonkcp742k@gmail.com>
2024-05-30 16:45:16 +0530

    feat(HistoryPeriodSelector): Add HistoryPeriodSelector component with tabs for selecting year or month timeframe.

---
+++ app/(dashboard)/_components/HistoryPeriodSelector.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { Period, Timeframe } from "@/lib/types";
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
+import { GetHistoryPeriodsResponseType } from "@/app/api/history-periods/route";
+import React from "react";
+import SkeletonWrapper from "@/components/SkeletonWrapper";
+import { useQuery } from "@tanstack/react-query";
+
+interface Props {
+    period: Period;
+    setPeriod: (period: Period) => void;
+    timeframe: Timeframe;
+    setTimeframe: (timeframe: Timeframe) => void;
+}
+
+function HistoryPeriodSelector({
+
+    timeframe,
+    setTimeframe,
+}: Props) {
+
+
+    return (
+        <div className="flex flex-wrap items-center gap-4">
+            {/* <SkeletonWrapper isLoading={historyPeriods.isFetching} fullWidth={false}> */}
+            <Tabs
+                value={timeframe}
+                onValueChange={(value) => setTimeframe(value as Timeframe)}
+            >
+                <TabsList>
+                    <TabsTrigger value="year">Year</TabsTrigger>
+                    <TabsTrigger value="month">Month</TabsTrigger>
+                </TabsList>
+            </Tabs>
+            {/* </SkeletonWrapper> */}
+
+        </div>
+    );
+}
+
+export default HistoryPeriodSelector;
+
+
