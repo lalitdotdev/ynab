73dde349012702b34a9ed02c474125e02471e954
Lalit sharma <geekymonkcp742k@gmail.com>
2024-05-30 16:52:12 +0530

    feat(Year and month Selector): Add year and month selectors for choosing history periods in the overview page.

--- app/(dashboard)/_components/HistoryPeriodSelector.tsx
+++ app/(dashboard)/_components/HistoryPeriodSelector.tsx
@@ -1,6 +1,13 @@
 "use client";
 
 import { Period, Timeframe } from "@/lib/types";
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select";
 import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 import { GetHistoryPeriodsResponseType } from "@/app/api/history-periods/route";
@@ -16,7 +23,8 @@ interface Props {
 }
 
 function HistoryPeriodSelector({
-
+    period,
+    setPeriod,
     timeframe,
     setTimeframe,
 }: Props) {
@@ -38,11 +46,99 @@ function HistoryPeriodSelector({
                     </TabsList>
                 </Tabs>
             </SkeletonWrapper>
-
+            <div className="flex flex-wrap items-center gap-2">
+                <SkeletonWrapper
+                    isLoading={historyPeriods.isFetching}
+                    fullWidth={false}
+                >
+                    <YearSelector
+                        period={period}
+                        setPeriod={setPeriod}
+                        years={historyPeriods.data || []}
+                    />
+                </SkeletonWrapper>
+                {timeframe === "month" && (
+                    <SkeletonWrapper
+                        isLoading={historyPeriods.isFetching}
+                        fullWidth={false}
+                    >
+                        <MonthSelector period={period} setPeriod={setPeriod} />
+                    </SkeletonWrapper>
+                )}
+            </div>
         </div>
     );
 }
 
 export default HistoryPeriodSelector;
 
+function YearSelector({
+    period,
+    setPeriod,
+    years,
+}: {
+    period: Period;
+    setPeriod: (period: Period) => void;
+    years: GetHistoryPeriodsResponseType;
+}) {
+    return (
+        <Select
+            value={period.year.toString()}
+            onValueChange={(value) => {
+                setPeriod({
+                    month: period.month,
+                    year: parseInt(value),
+                });
+            }}
+        >
+            <SelectTrigger className="w-[180px]">
+                <SelectValue />
+            </SelectTrigger>
+            <SelectContent>
+                {years.map((year) => (
+                    <SelectItem key={year} value={year.toString()}>
+                        {year}
+                    </SelectItem>
+                ))}
+            </SelectContent>
+        </Select>
+    );
+}
 
+function MonthSelector({
+    period,
+    setPeriod,
+}: {
+    period: Period;
+    setPeriod: (period: Period) => void;
+}) {
+    return (
+        <Select
+            value={period.month.toString()}
+            onValueChange={(value) => {
+                setPeriod({
+                    year: period.year,
+                    month: parseInt(value),
+                });
+            }}
+        >
+            <SelectTrigger className="w-[180px]">
+                <SelectValue />
+            </SelectTrigger>
+            <SelectContent>
+                {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11].map((month) => {
+                    const monthStr = new Date(period.year, month, 1).toLocaleString(
+                        "default",
+                        { month: "long" }
+                    );
+
+                    return (
+                        <SelectItem key={month} value={month.toString()}>
+                            {monthStr}
+                        </SelectItem>
+                    );
+                })}
+            </SelectContent>
+        </Select>
+    );
+}
