3a54421f9ec2ac3f134c882a62cc7441248035ca
Lalit sharma <geekymonkcp742k@gmail.com>
2024-05-31 12:33:20 +0530

    ✅ Implemented a page component to manage account settings and categories, including sections for setting the default currency and managing income and expense categories.

--- app/(dashboard)/_actions/categories.ts
+++ app/(dashboard)/_actions/categories.ts
@@ -1,6 +1,11 @@
 'use server';
 
-import { CreateCategorySchema, CreateCategorySchemaType } from '@/lib/validators/categories';
+import {
+  CreateCategorySchema,
+  CreateCategorySchemaType,
+  DeleteCategorySchema,
+  DeleteCategorySchemaType,
+} from '@/lib/validators/categories';
 
 import { currentUser } from '@clerk/nextjs/server';
 import db from '@/lib/db';
@@ -24,6 +29,28 @@ export async function CreateCategory(form: CreateCategorySchemaType) {
       name,
       icon,
       type,
+    },
+  });
+}
+
+export async function DeleteCategory(form: DeleteCategorySchemaType) {
+  const parsedBody = DeleteCategorySchema.safeParse(form);
+  if (!parsedBody.success) {
+    throw new Error('bad request');
+  }
+
+  const user = await currentUser();
+  if (!user) {
+    redirect('/sign-in');
+  }
+
+  return await db.category.delete({
+    where: {
+      name_userId_type: {
+        userId: user.id,
+        name: parsedBody.data.name,
+        type: parsedBody.data.type,
+      },
     },
   });
 }
