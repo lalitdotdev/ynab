9b83423989d1bc091e81adb5e2f51ad7c190c6d2
Lalit sharma <geekymonkcp742k@gmail.com>
2024-05-31 12:26:30 +0530

    API(history-data): Fetch monthly historical data grouped by day for a specific user and month from the database, creating a HistoryData array with expense and income values for each day of the month.

--- app/api/history-data/route.ts
+++ app/api/history-data/route.ts
@@ -51,7 +51,7 @@ async function getHistoryData(userId: string, timeframe: Timeframe, period: Peri
     case 'year':
       return await getYearHistoryData(userId, period.year);
     case 'month':
-    //   return await getMonthHistoryData(userId, period.year, period.month);
+      return await getMonthHistoryData(userId, period.year, period.month);
   }
 }
 
@@ -100,6 +100,51 @@ async function getYearHistoryData(userId: string, year: number) {
       month: i,
       expense,
       income,
+    });
+  }
+
+  return history;
+}
+
+async function getMonthHistoryData(userId: string, year: number, month: number) {
+  const result = await db.monthHistory.groupBy({
+    by: ['day'],
+    where: {
+      userId,
+      year,
+      month,
+    },
+    _sum: {
+      expense: true,
+      income: true,
+    },
+    orderBy: [
+      {
+        day: 'asc',
+      },
+    ],
+  });
+
+  if (!result || result.length === 0) return [];
+
+  const history: HistoryData[] = [];
+  const daysInMonth = getDaysInMonth(new Date(year, month));
+  for (let i = 1; i <= daysInMonth; i++) {
+    let expense = 0;
+    let income = 0;
+
+    const day = result.find((row) => row.day === i);
+    if (day) {
+      expense = day._sum.expense || 0;
+      income = day._sum.income || 0;
+    }
+
+    history.push({
+      expense,
+      income,
+      year,
+      month,
+      day: i,
     });
   }
 
