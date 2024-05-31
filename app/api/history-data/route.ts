88405cbea81dccfc0ac0cb180411ef4b2eaa952f
Lalit sharma <geekymonkcp742k@gmail.com>
2024-05-31 12:24:57 +0530

    API(history-data): Fetch yearly historical data grouped by month for a specific user and year from the database, creating a HistoryData array with expense and income values for each month.

--- app/api/history-data/route.ts
+++ app/api/history-data/route.ts
@@ -49,7 +49,7 @@ export type GetHistoryDataResponseType = Awaited<ReturnType<typeof getHistoryDat
 async function getHistoryData(userId: string, timeframe: Timeframe, period: Period) {
   switch (timeframe) {
     case 'year':
-    //   return await getYearHistoryData(userId, period.year);
+      return await getYearHistoryData(userId, period.year);
     case 'month':
     //   return await getMonthHistoryData(userId, period.year, period.month);
   }
@@ -62,3 +62,46 @@ type HistoryData = {
   month: number;
   day?: number;
 };
+
+async function getYearHistoryData(userId: string, year: number) {
+  const result = await db.yearHistory.groupBy({
+    by: ['month'],
+    where: {
+      userId,
+      year,
+    },
+    _sum: {
+      expense: true,
+      income: true,
+    },
+    orderBy: [
+      {
+        month: 'asc',
+      },
+    ],
+  });
+
+  if (!result || result.length === 0) return [];
+
+  const history: HistoryData[] = [];
+
+  for (let i = 0; i < 12; i++) {
+    let expense = 0;
+    let income = 0;
+
+    const month = result.find((row) => row.month === i);
+    if (month) {
+      expense = month._sum.expense || 0;
+      income = month._sum.income || 0;
+    }
+
+    history.push({
+      year,
+      month: i,
+      expense,
+      income,
+    });
+  }
+
+  return history;
+}
